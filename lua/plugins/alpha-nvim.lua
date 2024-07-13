d7864f0519921c468a7af238a4b3ada9d8c76005
Liam Goodacre <goodacre.liam@gmail.com>
2024-07-13 12:08:11 +0100

    start screen

---
+++ lua/plugins/alpha-nvim.lua
@@ -0,0 +1,37 @@
+return {
+  "goolord/alpha-nvim",
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+    dashboard.section.header.val = {
+
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⢀⣠⠶⠞⠛⠋⠉⠉⠉⠉⠙⠛⠳⠶⣄⡀⠀⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⣠⡾⠋⠀⠀⠀⠀⠀⠀   ⠀⠀⠀⠀⠀⠙⢷⣄⠀⠀⠀⠀⠀",
+      "⠀⠀⢀⡾⠋⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⡀⠀⠀⠀",
+      "⠀⢠⡞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢳⡄⠀⠀",
+      "⠀⣾⠁⠀⠀⠀⠀⠀⠀  ███   ⠀⠀⠀⠀⠀⠀⠀⠀⠈⣷⠀⠀",
+      "⢸⡏⠀⠀⠀⠀⠀⠀⠀   ██ █ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡇⠀",
+      "⢸⡇⠀⠀⠀⠀⠀⠀⠀   ████ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀",
+      "⢸⣇⠀⠀⠀⠀⠀⠀⠀    ██  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⡇⠀",
+      "⠀⢿⡀⠀⠀⠀⠀⠀⠀    █   ⠀⠀⠀⠀⠀⠀⠀⠀⢀⡿⠀⠀",
+      "⠀⠘⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⠃⠀⠀",
+      "⠀⠀⠈⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀   ⠀⠀⠀⠀⠀⠀⠀⣠⡾⠁⠀⠀⠀",
+      "⠀⠀⠀⠀⠙⢷⣄⠀⠀⠀⠀⠀⠀   ⠀⠀⠀⠀⠀⣠⡾⠋⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠈⠙⠶⢦⣤⣄⣀⣀⣀⣀⣠⣤⡴⠶⠋⠁⠀⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+
+    }
+
+    dashboard.section.buttons.val = {
+      dashboard.button("n",         "  Σ New      ", "<Cmd>ene<CR>"),
+      dashboard.button("Space e",   "  Σ Browse   ", "none"),
+      dashboard.button("x",         "  Σ Explore  ", "<Cmd>Ex<CR>"),
+      dashboard.button("Space f f", "  Σ Files    ", "none"),
+      dashboard.button("Space g g", "  Σ Git files", "none"),
+      dashboard.button("Space h k", "  Σ Help keys", "none"),
+    }
+
+    alpha.setup(dashboard.opts)
+  end,
+}
