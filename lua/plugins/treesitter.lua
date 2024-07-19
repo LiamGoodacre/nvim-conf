d4269495756651d2698ddfa6fde69bacac2903fb
Liam Goodacre <goodacre.liam@gmail.com>
2024-07-19 10:03:27 +0100

    refactoring treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,48 +9,6 @@ return {
 
   opts = function()
     local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      sync_install = true,
-      highlight = { enable = true },
-      indent = { enable = false },
-      incremental_selection = { enable = true },
-      ensure_installed = {
-        "bash",
-        "css",
-        "diff",
-        "dot",
-        "git_config",
-        "git_rebase",
-        "gitattributes",
-        "gitcommit",
-        "gitignore",
-        "haskell",
-        "html",
-        "javascript",
-        "jq",
-        "json",
-        "lua",
-        "make",
-        "markdown",
-        "mermaid",
-        "nix",
-        "passwd",
-        "purescript",
-        "python",
-        "query",
-        "regex",
-        "rst",
-        "scss",
-        "sql",
-        "terraform",
-        "tmux",
-        "toml",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "xml",
-        "yaml",
-      },
-    })
+    configs.setup(require("LiamGoodacre.treesitter").config)
   end
 }
