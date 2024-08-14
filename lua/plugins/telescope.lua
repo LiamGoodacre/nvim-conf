9423e85fee93b201c53398145ee5f0fe8dd4053e
Liam Goodacre <goodacre.liam@gmail.com>
2024-08-14 22:04:44 +0100

    telescope: toggle preview

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,13 @@
 return {
   'nvim-telescope/telescope.nvim', branch = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' }
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {
+    defaults = {
+      mappings = {
+        i = {
+          [""] = require("telescope.actions.layout").toggle_preview,
+        },
+      },
+    },
+  },
 }
