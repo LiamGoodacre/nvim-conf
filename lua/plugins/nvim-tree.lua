51533f992918dc1a541323753654b646e6f41e75
Liam Goodacre <goodacre.liam@gmail.com>
2024-09-01 18:25:33 +0100

    nvim-tree mappings

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -5,9 +5,31 @@ return {
   "nvim-tree/nvim-tree.lua",
   config = function()
     require("nvim-tree").setup({
+      on_attach = require("LiamGoodacre.mapping").on_nvim_tree_attach,
+      filters = {
+        enable = true,
+        dotfiles = true,
+      },
+      renderer = {
+        hidden_display = "simple",
+      },
       view = {
         width = 48,
         side = "left",
+        number = true,
+        relativenumber = true,
+        float = {
+          enable = false,
+          quit_on_focus_loss = true,
+          open_win_config = {
+            relative = "cursor",
+            border = "rounded",
+            width = 60,
+            height = 30,
+            row = 1,
+            col = 1,
+          },
+        },
       },
     })
   end,
