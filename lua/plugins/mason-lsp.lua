43ff5c70d4e117d5da43b4ed94c16ed52e7198bd
Liam Goodacre <liam@habito.com>
2025-05-21 20:28:02 +0100

    mason-org

--- lua/plugins/mason-lsp.lua
+++ lua/plugins/mason-lsp.lua
@@ -1,8 +1,8 @@
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
-    { "williamboman/mason.nvim", lazy = false, config = true },
-    { "williamboman/mason-lspconfig.nvim", lazy = false },
+    { "mason-org/mason.nvim", lazy = false, config = true },
+    { "mason-org/mason-lspconfig.nvim", lazy = false },
   },
   config = function()
     require("mason").setup()
