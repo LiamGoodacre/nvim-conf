2012b59a01992072e363bfe92e4c63b1b77b3d9d
Liam Goodacre <goodacre.liam@gmail.com>
2025-05-24 15:49:11 +0100

    haskell keywords include single quotes

--- lua/LiamGoodacre/languages/haskell.lua
+++ lua/LiamGoodacre/languages/haskell.lua
@@ -5,5 +5,19 @@ return {
     vim.filetype.add({ extension = { hs = haskell, lhs = haskell } })
     vim.treesitter.language.register('haskell', haskell)
     require("lspconfig").hls.setup({ filetypes = { haskell, 'cabal' } })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { haskell, 'cabal' },
+      callback = function()
+
+        -- @ = alphanumeric characters,
+        -- 39 = single quote
+        -- 48-57 = digits
+        -- _ = underscore
+        -- 192-255 = extended ASCII
+        vim.opt_local.iskeyword = "@,39,48-57,_,192-255"
+
+      end,
+    })
   end,
 }
