574dc71cffcad5c2a11bb8f5abdec7c7c3cf95b9
Liam Goodacre <goodacre.liam@gmail.com>
2025-07-18 19:49:49 +0100

    habito tweaks
    - disable purescriptls
    - disable hls, switch formatting to bormolu-format
    - pin copilot
    - disable zig

--- lua/LiamGoodacre/languages/purescript.lua
+++ lua/LiamGoodacre/languages/purescript.lua
@@ -1,9 +1,9 @@
 return {
-  lsps = {"purescriptls"},
+  lsps = {}, -- {"purescriptls"},
   setup = function()
     local purescript = "LiamGoodacre-purescript"
     vim.filetype.add({ extension = { purs = purescript } })
     vim.treesitter.language.register("purescript", purescript)
-    vim.lsp.config("purescriptls", { filetypes = { purescript } })
+    -- vim.lsp.config("purescriptls", { filetypes = { purescript } })
   end,
 }
