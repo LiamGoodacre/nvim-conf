6ed26f11e888c76acf19e14b4a160d9ed854e015
Liam Goodacre <goodacre.liam@gmail.com>
2025-07-18 22:25:40 +0100

    moving things around

---
+++ lua/LiamGoodacre/language.lua
@@ -0,0 +1,58 @@
+local M = {}
+
+M.setup = function()
+
+  -- Add to the ensure_installed list and restart.
+  -- Check installed with :TSInstallInfo
+  require("nvim-treesitter.configs").setup({
+    sync_install = true,
+    highlight = { enable = true },
+    indent = { enable = false },
+    incremental_selection = { enable = true },
+    ensure_installed = {
+      "bash",
+      "c",
+      "c_sharp",
+      "css",
+      "diff",
+      "dhall",
+      "dot",
+      "git_config",
+      "git_rebase",
+      "gitattributes",
+      "gitcommit",
+      "gitignore",
+      "haskell",
+      "html",
+      "javascript",
+      "jq",
+      "json",
+      "lua",
+      "make",
+      "markdown",
+      "mermaid",
+      "nix",
+      "passwd",
+      "purescript",
+      "python",
+      "query",
+      "regex",
+      "rst",
+      "scss",
+      "sql",
+      "terraform",
+      "tmux",
+      "toml",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
+    },
+  })
+
+  require("LiamGoodacre.util").setup_modules("LiamGoodacre.languages")
+
+end
+
+return M
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,60 +1,7 @@
--- Add to the ensure_installed list and restart.
--- Check installed with :TSInstallInfo
 return {
   "nvim-treesitter/nvim-treesitter",
 
   build = function()
     require("nvim-treesitter.install").update({ with_sync = true })()
   end,
-
-  opts = function()
-
-    require("nvim-treesitter.configs").setup({
-      sync_install = true,
-      highlight = { enable = true },
-      indent = { enable = false },
-      incremental_selection = { enable = true },
-      ensure_installed = {
-        "bash",
-        "c",
-        "c_sharp",
-        "css",
-        "diff",
-        "dhall",
-        "dot",
-        "git_config",
-        "git_rebase",
-        "gitattributes",
-        "gitcommit",
-        "gitignore",
-        "haskell",
-        "html",
-        "javascript",
-        "jq",
-        "json",
-        "lua",
-        "make",
-        "markdown",
-        "mermaid",
-        "nix",
-        "passwd",
-        "purescript",
-        "python",
-        "query",
-        "regex",
-        "rst",
-        "scss",
-        "sql",
-        "terraform",
-        "tmux",
-        "toml",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "xml",
-        "yaml",
-      },
-    })
-
-  end
 }
