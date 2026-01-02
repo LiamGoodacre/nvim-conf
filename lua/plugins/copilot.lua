d871823e96c852d867438e9ef23ba566653bd855
Liam Goodacre <liam@habito.com>
2026-01-02 17:10:29 +0000

    copilot: disable Tab mapping

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -5,6 +5,7 @@ return {
     -- We'll explicitly request a suggestion via mappings.
     -- See ``LiamGoodacre/mappings/copilot.lua``.
     vim.g.copilot_enabled = false
+    vim.g.copilot_no_tab_map = true
     vim.g.copilot_no_maps = true
   end,
 }
