1a1093f9a0c1d6fb2416fb4e3b653faa5ae9521b
Liam Goodacre <goodacre.liam@gmail.com>
2026-01-24 17:28:20 +0000

    starpls

--- lua/LiamGoodacre/languages/bzl.lua
+++ lua/LiamGoodacre/languages/bzl.lua
@@ -1,7 +1,7 @@
 return {
-  lsps = {"bzl"},
+  lsps = {"starpls"},
   setup = function()
-    vim.lsp.config("bzl", {
+    vim.lsp.config("starpls", {
       filetypes = { "bzl", "bazel", "bazelrc", "WORKSPACE" },
       root_dir = require("lspconfig.util").root_pattern("WORKSPACE", "WORKSPACE.bazel", ".git"),
     })
