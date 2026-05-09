986508c081c52c3097369e7a7d998041b0bab83e
Liam Goodacre <goodacre.liam@gmail.com>
2026-05-09 21:07:17 +0100

    switch nvim-telescope/nvim-telescope to neovim-telescope/nvim-telescope

--- lua/LiamGoodacre/language.lua
+++ lua/LiamGoodacre/language.lua
@@ -1,60 +1,71 @@
 local M = {}
 
+local treesitter_parsers = {
+  "bash",
+  "c",
+  "c_sharp",
+  "css",
+  "diff",
+  "dhall",
+  "dot",
+  "git_config",
+  "git_rebase",
+  "gitattributes",
+  "gitcommit",
+  "gitignore",
+  "haskell",
+  "html",
+  "javascript",
+  "jq",
+  "json",
+  "lua",
+  "make",
+  "markdown",
+  "mermaid",
+  "nix",
+  "passwd",
+  "purescript",
+  "python",
+  "query",
+  "regex",
+  "rst",
+  "rust",
+  "scss",
+  "sql",
+  "terraform",
+  "tmux",
+  "toml",
+  "typescript",
+  "vim",
+  "vimdoc",
+  "xml",
+  "yaml",
+  "zig",
+}
+
+local treesitter_filetypes = vim.list_extend({
+  "LiamGoodacre-bash",
+  "LiamGoodacre-csharp",
+  "LiamGoodacre-haskell",
+  "LiamGoodacre-lua",
+  "LiamGoodacre-markdown",
+  "LiamGoodacre-purescript",
+  "LiamGoodacre-python",
+  "LiamGoodacre-typescript",
+  "LiamGoodacre-zig",
+}, vim.deepcopy(treesitter_parsers))
+
 M.setup = function()
-
-  -- Add to the ensure_installed list and restart.
-  -- Check installed with :TSInstallInfo
-  require("nvim-treesitter.configs").setup({
-    sync_install = true,
-    highlight = { enable = true },
-    indent = { enable = false },
-    incremental_selection = { enable = true },
-    ensure_installed = {
-      "bash",
-      "c",
-      "c_sharp",
-      "css",
-      "diff",
-      "dhall",
-      "dot",
-      "git_config",
-      "git_rebase",
-      "gitattributes",
-      "gitcommit",
-      "gitignore",
-      "haskell",
-      "html",
-      "javascript",
-      "jq",
-      "json",
-      "lua",
-      "make",
-      "markdown",
-      "mermaid",
-      "nix",
-      "passwd",
-      "purescript",
-      "python",
-      "query",
-      "regex",
-      "rst",
-      "rust",
-      "scss",
-      "sql",
-      "terraform",
-      "tmux",
-      "toml",
-      "typescript",
-      "vim",
-      "vimdoc",
-      "xml",
-      "yaml",
-      "zig",
-    },
-  })
-
   require("LiamGoodacre.util").setup_modules("LiamGoodacre.languages")
 
+  require("nvim-treesitter").install(treesitter_parsers)
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = treesitter_filetypes,
+    callback = function()
+      vim.treesitter.start()
+    end,
+  })
 end
 
 return M
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-
-  build = function()
-    require("nvim-treesitter.install").update({ with_sync = true })()
-  end,
+  "neovim-treesitter/nvim-treesitter",
+  name = "nvim-treesitter",
+  dependencies = { "neovim-treesitter/treesitter-parser-registry" },
+  lazy = false,
+  build = ":TSUpdate",
 }
