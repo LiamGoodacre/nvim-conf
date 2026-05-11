09893ae756c8490355e9adc89cb9b93f80fa91a6
Liam Goodacre <liam@habito.com>
2026-05-11 08:53:02 +0100

    mid-search jump without commit

--- lua/LiamGoodacre/mappings/basics.lua
+++ lua/LiamGoodacre/mappings/basics.lua
@@ -6,6 +6,23 @@ M.setup = function()
 
   vim.keymap.set("n", "<leader>w", ":w<CR>")
 
+  vim.keymap.set("c", "<M-m>", function()
+    local cmdtype = vim.fn.getcmdtype()
+    if cmdtype ~= "/" and cmdtype ~= "?" then
+      return ""
+    end
+
+    local win = vim.api.nvim_get_current_win()
+    local pos = vim.api.nvim_win_get_cursor(win)
+    vim.schedule(function()
+      if vim.api.nvim_win_is_valid(win) then
+        pcall(vim.api.nvim_win_set_cursor, win, pos)
+      end
+    end)
+
+    return vim.api.nvim_replace_termcodes("<C-c>", true, false, true)
+  end, { expr = true })
+
 end
 
 return M
