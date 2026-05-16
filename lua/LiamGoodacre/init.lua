1280b7fb3e84ad8c29d754ae78cefe5f7e6dec78
Liam Goodacre <goodacre.liam@gmail.com>
2026-05-16 16:03:30 +0100

    core ui2 enable

--- lua/LiamGoodacre/init.lua
+++ lua/LiamGoodacre/init.lua
@@ -2,6 +2,7 @@ local M = {}
 
 --- Load the top-level config modules in startup order.
 M.setup = function()
+  require("vim._core.ui2").enable({})
 
   require("LiamGoodacre.util").setup_modules({
     "LiamGoodacre.setting",
