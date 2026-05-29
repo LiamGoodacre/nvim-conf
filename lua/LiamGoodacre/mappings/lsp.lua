f722cf125c726aefb4e1a49360b1cfeb899f3d9f
Liam Goodacre <goodacre.liam@gmail.com>
2026-05-29 21:36:12 +0100

    lsp completions

--- lua/LiamGoodacre/mappings/lsp.lua
+++ lua/LiamGoodacre/mappings/lsp.lua
@@ -5,6 +5,23 @@ M.setup = function()
   vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { desc = "Rename" })
   vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { desc = "Format" })
 
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      if not client or not client:supports_method("textDocument/completion") then
+        return
+      end
+
+      vim.lsp.completion.enable(true, client.id, args.buf, {
+        autotrigger = false,
+      })
+
+      vim.keymap.set("i", "<C-Space>", function()
+        vim.lsp.completion.get()
+      end, { buffer = args.buf })
+    end,
+  })
+
 end
 
 return M
