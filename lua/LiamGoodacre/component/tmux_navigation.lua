4b28d418891c41e6715b9184759f9c36611a495a
Liam <liamgoodacre@monzo.com>
2026-07-10 09:15:26 +0100

    tmux-navigation - setting is_vim tmux flag on enter,resume,focus

--- lua/LiamGoodacre/component/tmux_navigation.lua
+++ lua/LiamGoodacre/component/tmux_navigation.lua
@@ -1,5 +1,15 @@
 local M = {}
 
+M.before_load = function()
+  local grp = vim.api.nvim_create_augroup("tmux_nav_flag", { clear = true })
+  vim.api.nvim_create_autocmd({ "VimEnter", "VimResume", "FocusGained" }, {
+    group = grp,
+    command = "silent !tmux set-option -p @is_vim yes",
+  })
+  -- tmux config will take care of resetting on pane-focus-out
+end
+
+
 M.plugins = {
   { src = "https://github.com/alexghergh/nvim-tmux-navigation" },
 }
